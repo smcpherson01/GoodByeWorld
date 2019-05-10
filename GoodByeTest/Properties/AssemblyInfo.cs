2fa4de7ad5173237643824ac262cbb3ed595dc46
McPherson Shaun <SMcPherson@OLDMUTUAL.com>
2019-05-10 08:35:40 +0200

    Add project files.

---
+++ GoodByeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoodByeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Old Mutual")]
+[assembly: AssemblyProduct("GoodByeTest")]
+[assembly: AssemblyCopyright("Copyright © Old Mutual 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("659aeee3-171b-420a-bcb5-9acc694f7e5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GoodByeTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoodbyeWorld;
+
+namespace GoodByeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Goodbye World", Program.createMessage());
+        }
+    }
+}
